0b02123304015124ba33b18b341247926d580490
Damon Revoe <him@revl.org>
2016-11-23 23:30:50 -0500

    Add a comment with string_formatting implem. notes

--- src/string_formatting.h
+++ src/string_formatting.h
@@ -18,6 +18,29 @@
  *
  */
 
+// This file contains implementation of the printf-style string
+// formatting functions format_buffer() and format_buffer_va(),
+// which are also used by the string::format family of functions.
+//
+// The reason for implementing string formatting from scratch was
+// to avoid dealing with missing runtime library functions on
+// different systems as well as to have a single implementation
+// with known, predictable characteristics.
+//
+// Implementation notes:
+//
+// These formatting functions are designed to never require more than
+// a single pass of the format string parsing or more than a single
+// memory allocation regardless of the format string complexity.
+//
+// The 'string_formatting' structure below is what implements this goal.
+// Its methods call each other recursively to parse the format string
+// and estimate the length of the result string. Each recursive call
+// creates intermediate data on the stack. The deepest call allocates
+// the exact amount of memory needed to contain the entire result string.
+// When the recursion is then unwinded, each recursive call copies its
+// data to the designated place in that target buffer.
+
 namespace
 {
 	struct string_formatting
