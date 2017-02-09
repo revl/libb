862baa91fdb29306c2cdf99e48ebf86fc9bf2033
Damon Revoe <him@revl.org>
2017-02-08 21:17:53 -0500

    Fix opaque pointer implementation

--- include/b/opaque.h
+++ include/b/opaque.h
@@ -18,17 +18,50 @@
  *
  */
 
+#include "object.h"
 #include "ref.h"
 
 #ifndef B_OPAQUE_H
 #define B_OPAQUE_H
+
+B_BEGIN_NAMESPACE
+
+template <class Impl>
+class opaque_ref : public ref<object>
+{
+public:
+	opaque_ref();
+
+	opaque_ref(Impl* impl_ptr);
+
+	Impl* operator ->() const;
+};
+
+template <class Impl>
+inline opaque_ref<Impl>::opaque_ref()
+{
+}
+
+template <class Impl>
+inline opaque_ref<Impl>::opaque_ref(Impl* impl_ptr) :
+	ref<object>(impl_ptr)
+{
+}
+
+template <class Impl>
+inline Impl* opaque_ref<Impl>::operator ->() const
+{
+	return static_cast<Impl*>(ptr());
+}
+
+B_END_NAMESPACE
 
 // Declares an opaque pointer to the implementation class.
 // This macro is for use in the declaration of the interface class.
 #define B_OPAQUE \
     protected: \
     class impl; \
-    b::ref<impl> impl_ref; \
+    b::opaque_ref<impl> impl_ref; \
     public
 
 #endif /* !defined(B_OPAQUE_H) */
