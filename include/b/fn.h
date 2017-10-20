882e9792969de79c5098f466b15deb972e614480
Damon Revoe <him@revl.org>
2017-10-19 23:14:44 -0400

    Make wchar_to_char, char_to_wchar return size_t

--- include/b/fn.h
+++ include/b/fn.h
@@ -295,16 +295,16 @@ bool match_pattern(const wstring_view& input, const wstring_view& pattern);
 int compare_versions(const char* version1, const char* version2);
 
 // Converts sequence of wide characters to sequence of multibyte characters.
-inline void wchar_to_char(char* result, const wchar_t* source, size_t length)
+inline size_t wchar_to_char(char* result, const wchar_t* source, size_t length)
 {
-	::wcstombs(result, source, length);
+	return ::wcstombs(result, source, length);
 }
 
 // Converts a sequence of multibyte characters to a sequence of wide
 // characters.
-inline void char_to_wchar(wchar_t* result, const char* source, size_t length)
+inline size_t char_to_wchar(wchar_t* result, const char* source, size_t length)
 {
-	::mbstowcs(result, source, length);
+	return ::mbstowcs(result, source, length);
 }
 
 // Template functions for construction, destruction,
