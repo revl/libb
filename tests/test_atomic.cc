f85b3f0a1a45e5f06833975bdc8dd06eea45e6c4
Damon Revoe <him@revl.org>
2017-11-26 19:37:30 -0500

    Add a unit test for atomic

---
+++ tests/test_atomic.cc
@@ -0,0 +1,39 @@
+/*
+ * B: Common C++ library
+ * Copyright (C) 2002-2007, 2016, 2017 Damon Revoe
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <b/atomic.h>
+
+#include "unit_test.h"
+
+B_TEST_CASE(atomic)
+{
+	b::atomic refs = B_ATOMIC_STATIC_INIT(1);
+
+	++refs;
+
+	B_CHECK((int) refs == 2);
+
+	B_CHECK(--refs);
+	B_CHECK(!--refs);
+
+	refs = 1;
+
+	B_CHECK(!--refs);
+}
